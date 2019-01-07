19358575712b9203a1c851da4479be67e6dd8e0c
soundara rajan <soundaraj.thevan@gmail.com>
2019-01-07 12:55:39 +0530

    Update repeate.py

--- repeate.py
+++ repeate.py
@@ -1,12 +1,14 @@
-l1= list(input("enter the values\t"))
+N = input()
+l1= list(input())
 d= dict()
 l2=[]
-for i in l1:
-    if i in d:
-        d[i] = d[i]+1
-    else:
-        d[i] = 1
-    if d[i] > 1:
-        l2.append(i)
+if N == len(l1):
+    for i in l1:
+        if i in d:
+            d[i] = d[i]+1
+        else:
+            d[i] = 1
+        if d[i] > 1:
+            l2.append(i)
     
 print sorted(l2)
